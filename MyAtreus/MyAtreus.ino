6483547f1290580095eec67108e124f0e66acd58
Niklas Koponen <niklas.koponen@gmail.com>
2022-01-09 14:44:34 +0200

    Lisättiin qu näppäimiä.

--- MyAtreus/MyAtreus.ino
+++ MyAtreus/MyAtreus.ino
@@ -31,6 +31,7 @@
 #include "kaleidoscope/keyswitch_state.h"
 
 #define MO(n) ShiftToLayer(n)
+#define ML(n) MoveToLayer(n)
 #define TG(n) LockLayer(n)
 
 enum {
@@ -43,6 +44,18 @@ enum {
   M_SLASH_AND_QUESTIONMARK,
   M_QUOTE_AND_DOUBLEQUOTE,
   M_CARET,
+  M_PARENTHESIS,
+  M_ANGLES,
+  M_ALT_1,
+  M_ALT_2,
+  M_ALT_3,
+  M_ALT_4,
+  M_ALT_5,
+  M_ALT_6,
+  M_ALT_7,
+  M_ALT_8,
+  M_ALT_9,
+  M_ALT_0,
 };
 
 #define Key_Exclamation LSHIFT(Key_1)
@@ -82,39 +95,79 @@ enum {
 
 enum {
   QWERTY,
+  LAYER_1,
   FUN,
   UPPER,
-  SCAND_AND_F_KEYS
+  SCAND_AND_F_KEYS,
 };
 
 /* *INDENT-OFF* */
 KEYMAPS(
   [QWERTY] = KEYMAP_STACKED
   (
-       Key_Q                    ,Key_W     ,Key_E                    ,Key_R                      ,Key_T
-      ,LT(SCAND_AND_F_KEYS, A)  ,Key_S     ,Key_D                    ,Key_F                      ,Key_G
-      ,Key_Z                    ,Key_X     ,Key_C                    ,Key_V                      ,Key_B                        ,M(M_BACKTICK_AND_TILDE)
-      ,Key_Esc                  ,Key_Tab   ,Key_LeftGui              ,Key_LeftShift              ,Key_Backspace                ,Key_LeftControl
+       Key_Q                   ,Key_W       ,Key_E        ,Key_R                    ,Key_T
+      ,LT(SCAND_AND_F_KEYS, A) ,Key_S       ,Key_D        ,Key_F                    ,Key_G
+      ,Key_Z                   ,Key_X       ,Key_C        ,Key_V                    ,Key_B                        ,M(M_BACKTICK_AND_TILDE)
+      ,Key_Tab                 ,Key_LeftGui ,Key_LeftAlt  ,Key_LeftShift            ,Key_Backspace                ,CTL_T(Escape)
+      
 
-                                ,Key_Y     ,Key_U                    ,Key_I                      ,Key_O                        ,Key_P
-                                ,Key_H     ,Key_J                    ,Key_K                      ,Key_L                        ,M(M_SEMICOLON_AND_COLON)
-      ,M(M_BACKSLASH_AND_PIPE)  ,Key_N     ,LT(SCAND_AND_F_KEYS, M)  ,M(M_COMMA_AND_LESS_THAN)   ,M(M_PERIOD_AND_GREATER_THAN) ,M(M_SLASH_AND_QUESTIONMARK)
-      ,Key_LeftAlt              ,Key_Space ,MO(FUN)                  ,FI_Key_Minus               ,M(M_QUOTE_AND_DOUBLEQUOTE)   ,Key_Enter
+                               ,Key_Y       ,Key_U        ,Key_I                    ,Key_O                        ,Key_P
+                               ,Key_H       ,Key_J        ,Key_K                    ,Key_L                        ,M(M_SEMICOLON_AND_COLON)
+      ,M(M_BACKSLASH_AND_PIPE) ,Key_N       ,Key_M        ,M(M_COMMA_AND_LESS_THAN) ,M(M_PERIOD_AND_GREATER_THAN) ,M(M_SLASH_AND_QUESTIONMARK)
+      ,___                     ,Key_Space   ,MO(FUN)      ,FI_Key_Minus             ,M(M_QUOTE_AND_DOUBLEQUOTE)   ,Key_Enter
   ),
+  [LAYER_1] = KEYMAP_STACKED
+  (
+       Key_1  ,Key_2 ,Key_3 ,Key_4  ,Key_5
+      ,___    ,___   ,___   ,___    ,___
+      ,___    ,___   ,___   ,___    ,___    ,___
+      ,___    ,___   ,___   ,___    ,___    ,___
 
+              ,Key_6         ,Key_7         ,Key_8             ,Key_9          ,Key_0
+              ,Key_LeftArrow ,Key_DownArrow ,Key_UpArrow       ,Key_RightArrow ,___
+      ,___    ,___           ,___           ,___               ,___            ,___
+      ,___    ,ML(QWERTY)    ,___           ,___               ,___            ,___
+   ),
   [FUN] = KEYMAP_STACKED
   (
-       Key_Exclamation     ,FI_Key_At           ,Key_UpArrow   ,FI_Key_Dollar           ,Key_Percent
-      ,FI_Key_LeftParen    ,Key_LeftArrow       ,Key_DownArrow ,Key_RightArrow          ,FI_Key_RightParen
-      ,FI_Key_LeftBracket  ,FI_Key_RightBracket ,Key_Hash      ,FI_Key_LeftCurlyBrace   ,FI_Key_RightCurlyBrace   ,M(M_CARET)
-      ,TG(UPPER)           ,Key_Insert          ,Key_LeftGui   ,Key_LeftShift           ,Key_Delete               ,Key_LeftControl
+       Key_Exclamation    ,FI_Key_At           ,Key_UpArrow   ,FI_Key_Dollar         ,Key_Percent
+      ,FI_Key_LeftParen   ,Key_LeftArrow       ,Key_DownArrow ,Key_RightArrow        ,FI_Key_RightParen
+      ,FI_Key_LeftBracket ,FI_Key_RightBracket ,Key_Hash      ,FI_Key_LeftCurlyBrace ,FI_Key_RightCurlyBrace ,M(M_CARET)
+      ,TG(UPPER)          ,___                 ,___           ,___                   ,___                    ,___
 
-                           ,Key_PageUp          ,Key_7         ,Key_8                   ,Key_9                    ,Key_Backspace
-                           ,Key_PageDown        ,Key_4         ,Key_5                   ,Key_6                    ,FI_Key_Colon
-      ,FI_Key_And          ,FI_Key_Star         ,Key_1         ,Key_2                   ,Key_3                    ,FI_Key_Plus
-      ,Key_LeftAlt         ,Key_Space           ,___           ,Key_Period              ,Key_0                    ,FI_Key_Equals
+                          ,Key_PageUp          ,Key_7         ,Key_8                 ,Key_9                  ,Key_Percent
+                          ,Key_PageDown        ,Key_4         ,Key_5                 ,Key_6                  ,___
+      ,FI_Key_And         ,FI_Key_Star         ,Key_1         ,Key_2                 ,Key_3                  ,FI_Key_Plus
+      ,___                ,___                 ,___           ,___                   ,Key_0                  ,FI_Key_Equals
    ),
+      /*
+  [FUN] = KEYMAP_STACKED
+  (
+        ___  ,___ ,___ ,___  ,___
+       ,___  ,___ ,___ ,___  ,___
+       ,___  ,___ ,___ ,___  ,___  ,___
+       ,___  ,___ ,___ ,___  ,___  ,___
 
+             ,___ ,___ ,___  ,___  ,___
+             ,___ ,___ ,___  ,___  ,___
+       ,___  ,___ ,___ ,___  ,___  ,___
+       ,___  ,___ ,___ ,___  ,___  ,___
+   ),
+   */
+  /*
+  [FUN] = KEYMAP_STACKED
+  (
+       ___  ,___ ,___ ,___  ,___
+      ,___  ,___ ,___ ,___  ,___
+      ,___  ,___ ,___ ,___  ,___ ,___
+      ,___  ,___ ,___ ,___  ,___ ,___
+
+            ,___ ,___ ,___  ,___ ,___
+            ,___ ,___ ,___  ,___ ,___
+      ,___  ,___ ,___ ,___  ,___ ,___
+      ,___  ,___ ,___ ,___  ,___ ,___
+   ),
+   */
   [UPPER] = KEYMAP_STACKED
   (
        Key_Insert            ,Key_Home                 ,Key_UpArrow         ,Key_End         ,Key_PageUp
@@ -139,7 +192,8 @@ KEYMAPS(
                          ,Key_F4        ,Key_F5             ,Key_F6        ,FI_Key_ouml ,FI_Key_auml
       ,___               ,Key_F7        ,Key_F8             ,Key_F9        ,Key_Keypad3 ,___
       ,___               ,Key_F10       ,Key_F11            ,Key_F12       ,___         ,Key_KeypadEnter
-   )
+   ),
+
 )
 /* *INDENT-ON* */
 
@@ -230,9 +284,11 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   case M_QUOTE_AND_DOUBLEQUOTE:
     if (keyToggledOn(keyState)) {
       if (left_shift) {
-        kaleidoscope::Runtime.hid().keyboard().pressRawKey(Key_2);
+        return MACRO(T(2));
+        //kaleidoscope::Runtime.hid().keyboard().pressRawKey(Key_2);
       } else {
-        kaleidoscope::Runtime.hid().keyboard().pressRawKey(Key_Backslash);
+        return MACRO(T(Backslash));
+        //kaleidoscope::Runtime.hid().keyboard().pressRawKey(Key_Backslash);
       }
     }
     break;
@@ -246,6 +302,66 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
       kaleidoscope::Runtime.hid().keyboard().pressRawKey(Key_Space);
     }
     break;
+  case M_PARENTHESIS:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftShift), T(8), T(9), U(LeftShift), T(LeftArrow));
+    }
+  case M_ANGLES:
+    if (keyToggledOn(keyState)) {
+      return MACRO(T(NonUsBackslashAndPipe), D(LeftShift), T(NonUsBackslashAndPipe), U(LeftShift), T(LeftArrow));
+    }
+    break;
+    break;
+  case M_ALT_1:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(1));
+    }
+    break;
+  case M_ALT_2:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(2));
+    }
+    break;
+  case M_ALT_3:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(3));
+    }
+    break;
+  case M_ALT_4:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(4));
+    }
+    break;
+  case M_ALT_5:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(5));
+    }
+    break;
+  case M_ALT_6:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(6));
+    }
+    break;
+  case M_ALT_7:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(7));
+    }
+    break;
+  case M_ALT_8:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(8));
+    }
+    break;
+  case M_ALT_9:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(9));
+    }
+    break;
+  case M_ALT_0:
+    if (keyToggledOn(keyState)) {
+      return MACRO(D(LeftAlt), T(0));
+    }
+    break;
   default:
     break;
   }
@@ -256,6 +372,26 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
 void setup() {
   Kaleidoscope.setup();
   SpaceCadet.disable();
+  QUKEYS(
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 0), M(M_ALT_1)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 1), M(M_ALT_2)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 2), M(M_ALT_3)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 3), M(M_ALT_4)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 4), M(M_ALT_5)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 7), M(M_ALT_6)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 8), M(M_ALT_7)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 9), M(M_ALT_8)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 10), M(M_ALT_9)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(0, 11), M(M_ALT_0)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(2, 2), LSHIFT(Key_8)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(2, 9), LSHIFT(Key_9)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(2, 3), Key_NonUsBackslashAndPipe),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(2, 8), LSHIFT(Key_NonUsBackslashAndPipe)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(2, 1), RALT(Key_7)),
+      kaleidoscope::plugin::Qukey(QWERTY, KeyAddr(2, 10), RALT(Key_0)),
+      );
+  /*
+      */
 }
 
 void loop() {
